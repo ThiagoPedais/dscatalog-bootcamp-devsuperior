519c0f531ba3720272e6da731b74f9216e3bee4d
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-04-23 22:11:03 -0300

    h2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,17 +3,27 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-	private Long id;
-	private String name;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;	
 	
 	public Category() {
 		
 	}
-
 
 	public Category(Long id, String name) {
 		super();
@@ -21,21 +31,17 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
