af1f903ff98d9ef529af35315a50142c54596a36
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-04-28 17:57:13 -0300

    Add front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='container mt-1'>
+            <h1>App</h1>
+        </div>
+        
+    )
+}
+
+export default App;
