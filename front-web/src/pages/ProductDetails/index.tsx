f9c7edcda54f0dfcd2e6427dbfea4ca15b498479
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-06-17 19:59:17 -0300

    ProductDetails layout mobile

--- front-web/src/pages/ProductDetails/index.tsx
+++ front-web/src/pages/ProductDetails/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as ArrowIcon} from '../../core/assets/images/arrow.svg'
+import ProductPrice from '../Catalog/components/ProductPrice';
 import './style.scss'
 
 const ProductDetails = () => {
-  return (
-    <div>
-        <h1>Page Product Detail</h1>
-    </div>
-  );
+    return (
+        <section className="product-details-container">
+            <div className="card-base product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>Voltar</h2>
+                </div>
+
+                <div className="row">
+                    <div className="col-xl-6">
+
+                        <div className="img-container">
+                            <img src='https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg' alt='Nome do produto' />
+                        </div>
+
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.67}/>
+                        </div>
+
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default ProductDetails;
