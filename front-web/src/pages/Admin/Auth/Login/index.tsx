33a584c40aa828e58e8dcd1bbdbd69350edf2a0c
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-06-28 18:14:14 -0300

    React Hook Fomr first test

--- front-web/src/pages/Admin/Auth/Login/index.tsx
+++ front-web/src/pages/Admin/Auth/Login/index.tsx
@@ -1,44 +1,58 @@
 import { Link } from 'react-router-dom';
 import ButtonIcon from '../../../../core/components/Button';
- 
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import './style.scss';
- 
+
 const Login = () => {
-  return (
-    <div className="card-base login-card">
-      <h1>LOGIN</h1>
-      <form>
-        <div className="mb-4">
-          <input
-            type="text"
-            className="form-control base-input"
-            placeholder="Email"
-            name="username"
-          />
+
+    type FormData = {
+        username: string,
+        password: string,
+    };
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (formData: FormData) => {
+        console.log(formData);
+    };
+
+    return (
+        <div className="card-base login-card">
+            <h1>LOGIN</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-4">
+                    <input
+                        {...register("username")}
+                        type="text"
+                        className="form-control base-input"
+                        placeholder="Email"
+                        name="username"
+                    />
+                </div>
+                <div className="mb-2">
+                    <input
+                        {...register("password")}
+                        type="password"
+                        className="form-control base-input "
+                        placeholder="Password"
+                        name="password"
+                    />
+                </div>
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="Fazer login" />
+                </div>
+                <div className="signup-container">
+                    <span className="not-registered">Não tem Cadastro?</span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
+            </form>
         </div>
-        <div className="mb-2">
-          <input
-            type="password"
-            className="form-control base-input "
-            placeholder="Password"
-            name="password"
-          />
-        </div>
-        <Link to="/admin/auth/recover" className="login-link-recover">
-          Esqueci a senha
-        </Link>
-        <div className="login-submit">
-          <ButtonIcon text="Fazer login" />
-        </div>
-        <div className="signup-container">
-          <span className="not-registered">Não tem Cadastro?</span>
-          <Link to="/admin/auth/register" className="login-link-register">
-            CADASTRAR
-          </Link>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
- 
+
 export default Login;
