c0aae76e030cbb7b663a2112834ad6b3260c111a
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-07-18 13:16:48 -0300

    Form buttons styling

--- front-web/src/pages/Admin/Products/Form/index.tsx
+++ front-web/src/pages/Admin/Products/Form/index.tsx
@@ -10,23 +10,32 @@ export default function Form() {
 
         <form action="">
 
-          <div className="row">
+          <div className="row product-crud-inputs-container">
 
-            <div className="col-lg-6">
-              <input type="text" className='form-control base-input' />
-              <input type="text" className='form-control base-input' />
-              <input type="text" className='form-control base-input' />
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className='form-control base-input' />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className='form-control base-input' />
+              </div>
+              <div>
+                <input type="text" className='form-control base-input' />
+              </div>
+
             </div>
 
             <div className="col-lg-6">
-              <textarea name="" rows={10} className='form-control base-input' />
+              <div>
+                <textarea name="" rows={10} className='form-control base-input h-auto' />
+              </div>
             </div>
 
           </div>
 
-          <div>
-            <button className="btn btn-outline-danger">CANCELAR</button>
-            <button className="btn btn-outline-primary">SALVAR</button>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">CANCELAR</button>
+            <button className="btn btn-primary text-white product-crud-button">SALVAR</button>
 
           </div>
 
